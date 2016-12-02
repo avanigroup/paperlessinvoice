f4a6f5e6d853ea9b25a692f8a8a0ddb31cf9c90e
avanigroup <pola.kumar1@wipro.com>
2016-12-02 23:27:39 +0530

    Add files via upload

---
+++ SQLscripts/Sequence.sql
@@ -0,0 +1,32 @@
+  create sequence Customer_sequence
+  start with 00000001
+  increment by 1
+  maxvalue 99999999
+  cycle
+  cache 10; 
+  
+  create sequence Retailer_sequence
+  start with 00000001
+  increment by 1
+  maxvalue 99999999
+  cycle
+  cache 10; 
+  
+  create sequence ProductDetails_sequence
+  start with 00000001
+  increment by 1
+  maxvalue 99999999
+  cycle
+  cache 10;
+  
+  create sequence TransactionDetails_sequence
+  start with 0000000001
+  increment by 1
+  maxvalue 9999999999
+  cycle
+  cache 10;
+  
+
+
+  
+  
